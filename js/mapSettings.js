34df8d2408eedce69702e77f2d4f50ec1711c1c3
saikatmitra91 <saikatmitra91@gmail.com>
2015-03-28 16:20:20 +0530

    bug fixes and sorting function added

--- js/mapSettings.js
+++ js/mapSettings.js
@@ -28,6 +28,9 @@ var map = AmCharts.makeChart("mapDiv", {
 
 
 map.addListener("clickMapObject", function (event) {
+    if(!event.event){
+        return;
+    }
     var $target = $(event.event.target);
     var classname = $target.attr('class').split(/\s+/)[1];
     var splitArr = classname.split('-');
@@ -54,11 +57,18 @@ function fetchCityData(city){
     }
 
     for( i=0;i<details.lines.length;i++){
-        dataMap.outbound += parseInt(details.lines[i].balloonText); 
-        dataMap.cities.push(details.lines[i].doctor_city);
-        dataMap[details.lines[i].doctor_city] = details.lines[i].balloonText;
+        dataMap.outbound += parseInt(details.lines[i].balloonText);
+        var city = {};
+        city.name = details.lines[i].doctor_city;
+        city.count = parseInt(details.lines[i].balloonText);
+        dataMap.cities.push(city);
     }
+    dataMap.cities.sort(compareDesc);
     return dataMap;
+};
+
+function compareDesc(a,b){
+    return b.count-a.count;
 };
 
 function updateView(dataMap,city){
@@ -68,7 +78,10 @@ function updateView(dataMap,city){
     var i = 0;
     var htmlStr = "<ol>";
     for(i=0;i<dataMap.cities.length;i++){
-        htmlStr += "<li class='city-list-name'>" + dataMap.cities[i] + " : " + dataMap[dataMap.cities[i]] + "</li>";
+        htmlStr += "<li class='city-list-name'>" + dataMap.cities[i].name + " : " + dataMap.cities[i].count + "</li>";
+        if(i==3){
+            break;
+        }
     }
     htmlStr += "</ol>";
     $('.top-city-list').html(htmlStr);
